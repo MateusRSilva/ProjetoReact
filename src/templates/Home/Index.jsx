71d8f22e1d956ccbaa83ab59e2e835ddd3f6c0fe
Mattreis <mattreis07@gmail.com>
2022-05-16 21:42:32 -0300

    Atualização de cor

--- src/templates/Home/Index.jsx
+++ src/templates/Home/Index.jsx
@@ -1,80 +1,75 @@
 import './Style.css';
-import React, { Component } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { loadPosts } from '../../utils/load-posts';
 import { Posts } from '../../components/Posts/index';
 import { Button } from '../../components/Button/index'
 import { TextInput } from '../../components/textInput/Index';
-export class Home extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 10,
-    searchValue: ''
-  };
-  async componentDidMount() {
-    await this.loadPosts();
-  }
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
+
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState('');
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filterPosts = !!searchValue ?
+    allPosts.filter(post => {
+      return post.title.toLowerCase().includes(searchValue.toLowerCase());
+    })
+    :
+    posts;
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postAndPhoto = await loadPosts();
-    this.setState({
-      posts: postAndPhoto.slice(page, postsPerPage),
-      allPosts: postAndPhoto
-    });
-  }
-  loadMorePosts = () => {
-    const {
-      page,
-      postsPerPage,
-      allPosts,
-      posts
-    } = this.state;
+
+    setPosts(postAndPhoto.slice(page, postsPerPage));
+    setAllPosts(postAndPhoto);
+  }, [])
+
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
     posts.push(...nextPosts);
-    this.setState({ posts, page: nextPage });
+    setPosts(posts);
+    setPage(nextPage);
   }
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
-    this.setState({ searchValue: value })
+    setSearchValue(value);
   }
-  render() {
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-    const filterPosts = !!searchValue ?
-      allPosts.filter(post => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-      :
-      posts;
-    return (
-      <section className='container'>
-        <div className='Search-Container'>
-          {!!searchValue && (
-            <h1>Search Value : {searchValue}</h1>
-          )}
-          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
-        </div>
-        {filterPosts.length > 0 && (
-          <Posts
-            posts={filterPosts}
-          />
+
+  return (
+    <section className='container'>
+      <div className='Search-Container'>
+        {!!searchValue && (
+          <h1>Search Value : {searchValue}</h1>
         )}
-        {filterPosts.length === 0 && (
-          <p>Não existe posts</p>
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
+      </div>
+      {filterPosts.length > 0 && (
+        <Posts
+          posts={filterPosts}
+        />
+      )}
+      {filterPosts.length === 0 && (
+        <p>Não existe posts</p>
+      )}
+      <div className='button-container'>
+        {!searchValue && (
+          <>
+            <Button text="Load more posts"
+              clicado={loadMorePosts}
+              disabled={noMorePosts}
+            />
+          </>
         )}
-        <div className='button-container'>
-          {!searchValue && (
-            <>
-              <Button text="Load more posts"
-                clicado={this.loadMorePosts}
-                disabled={noMorePosts}
-              />
-            </>
-          )}
-        </div>
-      </section>
-    )
-  }
+      </div>
+    </section>
+  )
 }
